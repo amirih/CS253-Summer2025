f84f1ec56a9b0936ab6739d8a1e4194a96dff5f8
amirih <hossein.amiri.enterprise@gmail.com>
2025-07-25 11:03:36 -0400

    Nsort

---
+++ NSort.java
@@ -0,0 +1,24 @@
+
+public class NSort extends SortAlgorithm implements ArrayPrinter {
+
+    @Override
+    public void printArray(int[] array) {
+        System.out.println("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println("]");
+
+    }
+
+    @Override
+    public int[] sort(int[] array) {
+
+        return array;
+    }
+
+    @Override
+    public String getName() {
+        return "NSort";
+    }
+}
